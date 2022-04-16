48536c854591c2129f5cab531f35eaaeaf40dd20
relativemodder <kezin636@gmail.com>
2022-04-16 16:00:06 +0300

    please

--- app.py
+++ app.py
@@ -8,12 +8,9 @@ app.config['SECRET_KEY'] = 'remoteprintkeyyoyoyoyoyoyo'
 @app.route('/')
 def index():
     return render_template('index.html', title="Главная")
-
-
-
-
-
-
+@app.route('/profile')
+def profile_page():
+    return render_template('profile.html', title="Профиль")
 
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
